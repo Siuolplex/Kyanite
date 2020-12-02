50c68fa7bb8a0813b1dd00a3df26c92ee87295a3
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2020-12-02 13:19:18 -0600

    Update NetheriteKykiniteArmorMaterials.java

--- src/main/java/com/dev/siuolplex/armor/NetheriteKykiniteArmorMaterials.java
+++ src/main/java/com/dev/siuolplex/armor/NetheriteKykiniteArmorMaterials.java
@@ -66,7 +66,7 @@ public class NetheriteKykiniteArmorMaterials implements BaseKykiniteArmorMateria
     }
     @Override
     public float getArmorPercentBoost(){
-        return 0.0F
+        return 0.0F;
     }
 
 }
