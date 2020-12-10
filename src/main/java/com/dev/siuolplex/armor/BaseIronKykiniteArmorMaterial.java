d94ea19e7b2fac419ccc9a96e22520f6ce8304ca
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2020-12-09 20:07:55 -0600

    Create BaseIronKykiniteArmorMaterial.java

---
+++ src/main/java/com/dev/siuolplex/armor/BaseIronKykiniteArmorMaterial.java
@@ -0,0 +1,8 @@
+package com.dev.siuolplex.armor;
+
+import net.minecraft.item.ArmorMaterial;
+import com.dev.siuolplex.armor.BaseKykiniteArmorMaterial
+
+public interface BaseIronKykiniteArmorMaterial extends BaseKykiniteArmorMaterial {
+    float getArmorPercentBoost();
+}
