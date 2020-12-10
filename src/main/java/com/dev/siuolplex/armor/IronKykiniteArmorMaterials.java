d2703d6948df8cb29231bc6e206c365c63f54b91
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2020-12-09 20:06:52 -0600

    Update IronKykiniteArmorMaterials.java

--- src/main/java/com/dev/siuolplex/armor/IronKykiniteArmorMaterials.java
+++ src/main/java/com/dev/siuolplex/armor/IronKykiniteArmorMaterials.java
@@ -6,7 +6,7 @@ import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
-public class IronKykiniteArmorMaterials implements BaseKykiniteArmorMaterial {
+public class IronKykiniteArmorMaterials implements BaseIronKykiniteArmorMaterial {
 
     private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
     private static final int[] PROTECTION_VALUES = new int[]{2, 5, 6, 2};
